3984b4e818b3e09aaac42c042a709d02921ab157
LJY9347 <leejyeyoung@naver.com>
2024-04-01 14:59:21 +0900

    test

--- lab07_inheritance/src/com/itwill/inheritance04/InheritanceMain04.java
+++ lab07_inheritance/src/com/itwill/inheritance04/InheritanceMain04.java
@@ -6,9 +6,32 @@ package com.itwill.inheritance04;
  * final 필드: 반드시 한 번은 명시적으로 초기화해야 하고, 이후에는 값을 변경할 수 없는 필드.
  *  (1) final 필드를 선언과 동시에 초기화 
  *  (2) final 필드를 초기화 할 수 있는 아규먼트를 갖는 생성자를 작성.
- * final 메서드: 변경할 수 없는 메서드.
+ * final 메서드: 변경할 수 없는 메서드. 재정의(override)할 수 없는 메서드.
  * final 클래스: 변경할 수 없는 클래스.
  */
+class A {
+	public void test1 () {
+		System.out.println("test1");
+	}
+	
+	public final void test2 () {
+		System.out.println("test2");
+	}
+}
+
+class B extends A {
+	@Override // 상위 클래스의 final이 아닌 메서드는 override 할 수 있음.
+	public void test1() {
+		System.out.println("B test1");
+	}
+	
+	// 상위 클래스에서 final로 선언된 메서드는 하위 클래스에서 override 할 수 없음.
+//	public void test2() {
+//		System.out.println("...");
+//	}
+}
+
+
 public class InheritanceMain04 {
 
 	public static void main(String[] args) {
