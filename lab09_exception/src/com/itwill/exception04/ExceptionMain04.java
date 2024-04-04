70df4112f8890e0de52a8971a9cce21bd341321e
LJY9347 <leejyeyoung@naver.com>
2024-04-04 16:06:22 +0900

    4/4

--- lab09_exception/src/com/itwill/exception04/ExceptionMain04.java
+++ lab09_exception/src/com/itwill/exception04/ExceptionMain04.java
@@ -7,6 +7,8 @@ package com.itwill.exception04;
  *   		|__ Exception
  *   				|__ RuntimeException
  *   							|__ ArithmeticException, NumberFormatException, NullPointException, ...
+ *   catch 블록이 여러개인 경우, 하위 타입 의 예외를 먼저 catch하고,
+ *   상위 타입의 예뢰를 나중에 catch해야 함.
  */
 
 public class ExceptionMain04 {
@@ -15,7 +17,14 @@ public class ExceptionMain04 {
 		// finally: 예외 상황 발생 여부와 상관 없이 항상 실행되는 블록.
 		// try 또는 catch 블록에 return 문장이 있더라도,
 		// 항상 finally 블록이 실행된 후에 return 문장이 실행됨!!
-		// catch를 여러개 작성할 경우 먼저 작성하는 catch에서 상위타입 Exception을 먼저 작성할경우 컴파일에러.
+		try {
+			 
+		} catch (ArithmeticException e) {
+			
+		} catch (Exception e) {
+			// 다형성: SuperType var = new SubType();
+			// Exception 클래스를 상속하는 모든 종류의 예외를 잡을 수 있음.
+		}
 		
 		try {
 			int x = 100;
