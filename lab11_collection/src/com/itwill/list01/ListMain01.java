da36d8954166b3c59c3317649426beb81ab7c0ca
LJY9347 <leejyeyoung@naver.com>
2024-04-05 15:38:49 +0900

    4/5

---
+++ lab11_collection/src/com/itwill/list01/ListMain01.java
@@ -0,0 +1,14 @@
+package com.itwill.list01;
+
+import java.util.ArrayList;
+
+public class ListMain01 {
+
+	public static void main(String[] args) {
+		// 정수들을 저장하는 ArrayList를 생성.
+		ArrayList<Integer> numbers = new ArrayList<>();
+		
+		
+	}
+
+}
