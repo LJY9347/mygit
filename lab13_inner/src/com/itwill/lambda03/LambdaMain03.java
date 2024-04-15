876ca440f62a66cb50aca93c4fa32fd5c4693d84
LJY9347 <leejyeyoung@naver.com>
2024-04-15 16:02:18 +0900

    4/15

--- lab13_inner/src/com/itwill/lambda03/LambdaMain03.java
+++ lab13_inner/src/com/itwill/lambda03/LambdaMain03.java
@@ -1,6 +1,7 @@
 package com.itwill.lambda03;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.function.Predicate;
@@ -42,6 +43,37 @@ public class LambdaMain03 {
 		// Stream 사용해서, numbers의 원소들 중 홀수들의 제곱을 저장하는 리스트를 만들고 출력
 		List<Integer> odds1 = numbers.stream().filter((x) -> x % 2 == 1).map((x) -> x * x).toList();
 		System.out.println(odds1);
+		
+		List<String> languages = Arrays.asList("java", "javascript", "python", "c", "kotlin", "swift");
+		
+		// languages의 원소들 중 문자열 길이 5글자 이상인 원소들의 리스트
+		List<String> length = languages.stream().filter((x) -> x.length() >= 5).toList();
+		System.out.println(length);		
+		// languages의 문자열을 대문자로 변환한 리스트
+		List<String> upper = languages.stream().map((x) -> x.toUpperCase()).toList();
+		System.out.println(upper);
+		// languages의 원소들 중 길이 5글자 이상인 문자열을 대문자로 변환한 리스트
+		List<String> lengthUpper = languages.stream()
+				.filter((x) -> x.length() >=5 )
+//				.map((x) -> x.toUpperCase())
+				.map(String::toUpperCase) // 메서드 참조 람다
+				.toList();
+		System.out.println(lengthUpper);
+				
+		/* 
+		 * 람다: (파라미터 선언) -> 리턴값
+		 * "메서드 참조(method reference)"를 사용한 람다 표현식
+		 * (1) 람다가 아규먼트를 1개만 갖고,
+		 * 람다의 리턴값이 그 아규먼트에서 "파라미터가 없는 메서드"를 호출한 경우:
+		 * 	   x -> x.toUpperCase()  59번
+		 * 	   String::toUpperCase
+		 * (2) 람다가 아규먼트를 1개만 갖고,
+		 * 람다의 구현부가 메서드 1개 호출이고, 그 메서드가 람다의 아규먼트를 전달받는 경우:
+		 *     x -> System.out.println(x)
+		 *     System.out::println
+		 */
+//		languages.forEach((x) -> System.out.println(x));
+		languages.forEach(System.out::println);
 	}
 
 }
