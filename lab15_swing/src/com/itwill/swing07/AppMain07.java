0fbb6e0de8e83d044b256f2e703287ea63bb4785
LJY9347 <leejyeyoung@naver.com>
2024-04-17 18:00:56 +0900

    4/16

--- lab15_swing/src/com/itwill/swing07/AppMain07.java
+++ lab15_swing/src/com/itwill/swing07/AppMain07.java
@@ -11,113 +11,115 @@ import java.awt.event.ActionEvent;
 
 public class AppMain07 {
 
-	private JFrame frame;
-	private JButton btnMsgDlg;
-	private JButton btnConfirmDlg;
-	private JButton btnInputDlg;
-	private JButton btcCustomDlg;
+    private JFrame frame;
+    private JButton btnMsgDlg;
+    private JButton btnConfirmDlg;
+    private JButton btnInputDlg;
+    private JButton btnCustomDlg;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					AppMain07 window = new AppMain07();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    AppMain07 window = new AppMain07();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	/**
-	 * Create the application.
-	 */
-	public AppMain07() {
-		initialize();
-	}
+    /**
+     * Create the application.
+     */
+    public AppMain07() {
+        initialize();
+    }
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 640, 648);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-
-		btnMsgDlg = new JButton("Message Dialog");
-		btnMsgDlg.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// 메시지 다이얼로그 보여주기
-//				JOptionPane.showMessageDialog(frame, "안녕하세요.");
-				JOptionPane.showMessageDialog(frame, // 부모 컴포넌트
-						"안녕하세요, Swing!", // 다이얼로그 메시지
-						"메시지", // 다이얼로그 타이틀(제목)
-						JOptionPane.INFORMATION_MESSAGE); // 메시지 타입 -> 메시지 아이콘
-			}
-		});
-		btnMsgDlg.setFont(new Font("D2Coding", Font.PLAIN, 32));
-		btnMsgDlg.setBounds(29, 23, 562, 106);
-		frame.getContentPane().add(btnMsgDlg);
-
-		btnConfirmDlg = new JButton("Confirm Dialog");
-		btnConfirmDlg.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// Confirm(확인) 다이얼로그 보여주기
-//				int result = JOptionPane.showConfirmDialog(frame, "정말 삭제할까요?");
-				int result = JOptionPane.showConfirmDialog(frame, // 부모 컴포넌트
-						"정말 삭제할까요?", // 메시지
-						"삭제 확인", // 타이틀
-						JOptionPane.YES_NO_OPTION, // public static final 상수 // 옵션 타임(버튼 종류, 개수)
-						JOptionPane.QUESTION_MESSAGE); // 메시지 타입
-
-				btnConfirmDlg.setText("confirm=" + result);
-			}
-		});
-		btnConfirmDlg.setFont(new Font("D2Coding", Font.PLAIN, 32));
-		btnConfirmDlg.setBounds(29, 139, 562, 106);
-		frame.getContentPane().add(btnConfirmDlg);
-
-		btnInputDlg = new JButton("Input Dialog");
-		btnInputDlg.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// 입력 다이얼로그 보여주기
-
-				final String[] selections = {"인스타", "페이스북", "트위터", "유튜브"};
-				Object result = JOptionPane.showInputDialog(
-						frame, // 부모 컴포넌트
-						"검색어 입력", // 메시지
-						"검색창", // 타이틀
-						JOptionPane.PLAIN_MESSAGE, // 메시지 타입 - 메시지 기본 아이콘
-						null, // 아이콘
-						selections, // 선택할 값들
-						selections[1]); // 초기 선택값
-
-				btnInputDlg.setText("입력: " + result);
-			}
-		});
-		btnInputDlg.setFont(new Font("D2Coding", Font.PLAIN, 32));
-		btnInputDlg.setBounds(29, 255, 562, 106);
-		frame.getContentPane().add(btnInputDlg);
-		
-		btcCustomDlg = new JButton("Custom Dialog");
-		btcCustomDlg.addActionListener(new ActionListener() {
-			@Override	
-			public void actionPerformed(ActionEvent e) {
-				// 내가 만든 다이얼로그 보여주기
-				MyDialog.showMyDialog();
-			}
-		});
-		btcCustomDlg.setFont(new Font("D2Coding", Font.PLAIN, 32));
-		btcCustomDlg.setBounds(29, 371, 562, 106);
-		frame.getContentPane().add(btcCustomDlg);
-	}
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 450, 478);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+        
+        btnMsgDlg = new JButton("Message Dialog");
+        btnMsgDlg.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // 메시지 다이얼로그 보여주기
+//                JOptionPane.showMessageDialog(frame, "안녕하세요.");
+                JOptionPane.showMessageDialog(
+                        frame,                      // 부모 컴포넌트
+                        "안녕하세요, Swing!",       // 다이얼로그 메시지
+                        "메시지",                   // 다이얼로그 타이틀(제목)
+                        JOptionPane.INFORMATION_MESSAGE); // 메시지 타입 -> 메시지 아이콘
+            }
+        });
+        btnMsgDlg.setFont(new Font("D2Coding", Font.BOLD, 32));
+        btnMsgDlg.setBounds(12, 10, 410, 64);
+        frame.getContentPane().add(btnMsgDlg);
+        
+        btnConfirmDlg = new JButton("Confirm Dialog");
+        btnConfirmDlg.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Confirm(확인) 다이얼로그 보여주기
+//                int result = JOptionPane.showConfirmDialog(frame, "정말 삭제할까요?");
+                int result = JOptionPane.showConfirmDialog(
+                        frame,                         // 부모 컴포넌트
+                        "정말 삭제할까요?",            // 메시지
+                        "삭제 확인",                   // 타이틀
+                        JOptionPane.YES_NO_OPTION,     // 옵션 타입(버튼 종류, 개수)
+                        JOptionPane.QUESTION_MESSAGE); // 메시지 타입
+                
+                btnConfirmDlg.setText("confirm=" + result);
+            }
+        });
+        btnConfirmDlg.setFont(new Font("D2Coding", Font.BOLD, 32));
+        btnConfirmDlg.setBounds(12, 84, 410, 64);
+        frame.getContentPane().add(btnConfirmDlg);
+        
+        btnInputDlg = new JButton("Input Dialog");
+        btnInputDlg.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // 입력 다이얼로그 보여주기
+//                String result = JOptionPane.showInputDialog(frame, "검색어");
+                
+                final String[] selections = {"인*", "얼굴장부", "X", "너튜브"};
+                Object result = JOptionPane.showInputDialog(
+                        frame, // 부모 컴포넌트
+                        "검색어 입력", // 메시지
+                        "검색어", // 타이틀
+                        JOptionPane.PLAIN_MESSAGE, // 메시지 타입 - 메시지 기본 아이콘
+                        null, // 아이콘
+                        selections, // 선택할 값들
+                        selections[1]); // 초기 선택값
+                btnInputDlg.setText("입력: " + result);
+            }
+        });
+        btnInputDlg.setFont(new Font("D2Coding", Font.BOLD, 32));
+        btnInputDlg.setBounds(12, 158, 410, 64);
+        frame.getContentPane().add(btnInputDlg);
+        
+        btnCustomDlg = new JButton("Custom Dialog");
+        btnCustomDlg.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // 내가 만든 다이얼로그 보여주기
+                MyDialog.showMyDialog(frame);
+            }
+        });
+        btnCustomDlg.setFont(new Font("D2Coding", Font.BOLD, 32));
+        btnCustomDlg.setBounds(12, 232, 410, 64);
+        frame.getContentPane().add(btnCustomDlg);
+    }
 
 }
--- lab15_swing/src/com/itwill/swing07/MyDialog.java
+++ lab15_swing/src/com/itwill/swing07/MyDialog.java
@@ -1,6 +1,7 @@
 package com.itwill.swing07;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.FlowLayout;
 
 import javax.swing.JButton;
@@ -13,81 +14,85 @@ import javax.swing.JTextField;
 
 public class MyDialog extends JDialog {
 
-	private static final long serialVersionUID = 1L;
-	private final JPanel contentPanel = new JPanel();
-	private JPanel buttonPanel;
-	private JButton cancelButton;
-	private JButton okButton;
-	private JTextField textField;
-	private JButton btnNewButton;
-	private JLabel lblNewLabel;
+    private static final long serialVersionUID = 1L;
+    private final JPanel contentPanel = new JPanel();
+    private JPanel buttonPanel;
+    private JButton cancelButton;
+    private JButton okButton;
+    private JLabel lblNewLabel;
+    private JTextField textField;
+    private JButton btnNewButton;
+    
+    // 부모 컴포넌트를 저장하기 위한 필드
+    private Component parentComponent;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void showMyDialog() {
-		try {
-			MyDialog dialog = new MyDialog();
-//			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    /**
+     * Launch the application.
+     */
+    public static void showMyDialog(Component parentComponent) {
+        try {
+            MyDialog dialog = new MyDialog(parentComponent);
+//            dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+            dialog.setVisible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * Create the dialog.
-	 */
-	public MyDialog() {
-		initialize();
-	}
+    /**
+     * Create the dialog.
+     */
+    public MyDialog(Component parentComponent) {
+    	this.parentComponent = parentComponent;
+        initialize();
+    }
 
-	private void initialize() {
-		// 다이얼로그의 좌표(x, y)와 크기(width, height)
-		setBounds(100, 100, 647, 644);
-		
-		// setBounds의 (x, y) 좌표를 무시하고 항상 스크린의 가운데에 위치.
-		setLocationRelativeTo(null);
+    private void initialize() {
+        // 다이얼로그의 좌표(x, y)와 크기(width, height)
+        setBounds(100, 100, 600, 400);
+        
+        // setBounds의 (x, y) 좌표를 무시하고 부모 컴포넌트에 상대적으로 배치(부모의 가운데 위치)
+        // 아규먼트가 null인 경우에는 화면 중앙에 배치.
+        setLocationRelativeTo(parentComponent);
 
-		// 다이얼로그의 닫기 버튼(X)의 기본 동작을 설정:
-		// DISPOSE_ON_CLOSE: 현재 다이얼로그만 닫고, 메인 프로세스는 계속 실행
-		// EXIT_ON_CLOSE: 현재 다이얼로그와 함께 메인 프로세스까지 종료
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        // 다이얼로그의 닫기 버튼(X)의 기본 동작을 설정:
+        // DISPOSE_ON_CLOSE: 현재 다이얼로그만 닫고, 메인 프로세스는 계속 실행
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
-		// getContentPane(): 컨텐트 영역 & 버튼 영역
-		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.setLayout(null);
-		
-		lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(22, 23, 121, 54);
-		contentPanel.add(lblNewLabel);
-		
-		textField = new JTextField();
-		textField.setBounds(12, 130, 260, 89);
-		contentPanel.add(textField);
-		textField.setColumns(10);
-		
-		btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(22, 258, 121, 89);
-		contentPanel.add(btnNewButton);
+        // getContentPane(): 컨텐트 영역 & 버튼 영역
+        getContentPane().setLayout(new BorderLayout());
+        contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+        getContentPane().add(contentPanel, BorderLayout.CENTER);
+        contentPanel.setLayout(null);
+        
+        lblNewLabel = new JLabel("New label");
+        lblNewLabel.setBounds(12, 10, 103, 43);
+        contentPanel.add(lblNewLabel);
+        
+        textField = new JTextField();
+        textField.setBounds(12, 70, 308, 83);
+        contentPanel.add(textField);
+        textField.setColumns(10);
+        
+        btnNewButton = new JButton("New button");
+        btnNewButton.setBounds(12, 163, 178, 69);
+        contentPanel.add(btnNewButton);
 
-		buttonPanel = new JPanel();
-		buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
-		getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+        buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        getContentPane().add(buttonPanel, BorderLayout.SOUTH);
 
-		okButton = new JButton("OK");
-		okButton.setFont(new Font("D2Coding", Font.PLAIN, 20));
-		okButton.setActionCommand("OK");
-		buttonPanel.add(okButton);
-		getRootPane().setDefaultButton(okButton);
+        cancelButton = new JButton("Cancel");
+        cancelButton.setFont(new Font("D2Coding", Font.PLAIN, 32));
+        cancelButton.setActionCommand("Cancel");
+        buttonPanel.add(cancelButton);
 
-		cancelButton = new JButton("Cancel");
-		cancelButton.setFont(new Font("D2Coding", Font.PLAIN, 20));
-		cancelButton.setActionCommand("Cancel");
-		buttonPanel.add(cancelButton);
+        okButton = new JButton("OK");
+        okButton.setFont(new Font("D2Coding", Font.PLAIN, 32));
+        okButton.setActionCommand("OK");
+        buttonPanel.add(okButton);
+        getRootPane().setDefaultButton(okButton);
 
-	} // end initialize
+    } // end initialize()
 
 }
